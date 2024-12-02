6cb347c869ac822ea42bcd945d96082c34c5bc01
Prog-san <DieSanR@gmail.com>
2024-12-02 04:25:40 -0500

    feat: Add principal information of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// Componentes de la landing page
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
