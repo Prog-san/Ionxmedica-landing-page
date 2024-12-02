263aacc7de9699d0108424f7745ceaa10bf0754d
Prog-san <DieSanR@gmail.com>
2024-12-02 05:11:25 -0500

    feat: navbar added
    Se desarrolla el navbar de la pagina web con una animación para que quede activa cuando se navegue.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  lastScrollTop = 0;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    const toolbar = document.querySelector('.custom-toolbar') as HTMLElement;
+    const toolbarBottom = document.querySelector('.custom-toolbar-bottom') as HTMLElement;
+
+    if (currentScroll > this.lastScrollTop) {
+      // Scroll down
+      toolbar.style.top = '-50px';
+      toolbarBottom.style.top = '0';
+    } else {
+      // Scroll up
+      toolbar.style.top = '0';
+      toolbarBottom.style.top = '50px';
+    }
+    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+  }
 }
